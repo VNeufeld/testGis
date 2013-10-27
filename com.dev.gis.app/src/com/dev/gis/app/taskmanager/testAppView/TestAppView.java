4d84f21cab36b299c523d6b6d209e1a5c03cb927
Valeri Neufeld <valeri.neufeld@arcor.de>
2013-10-27 22:57:54 +0100

    add some controls

--- com.dev.gis.app/src/com/dev/gis/app/taskmanager/testAppView/TestAppView.java
+++ com.dev.gis.app/src/com/dev/gis/app/taskmanager/testAppView/TestAppView.java
@@ -1,8 +1,18 @@
 package com.dev.gis.app.taskmanager.testAppView;
 
+import java.util.Calendar;
+
+import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
@@ -11,20 +21,77 @@ import com.dev.gis.task.execution.api.ITaskResult;
 
 public class TestAppView extends TaskViewAbstract {
 	public static final String ID = "com.dev.gis.app.task.TestAppView";
-
+	private StringFieldEditor city;
+	private StringFieldEditor airport;
 	@Override
 	public void createPartControl(Composite parent) {
-		Composite container = new Composite(parent, SWT.NONE);
-		container.setLayout(new FillLayout());
-		
-		Label label = new Label(container, SWT.BORDER);
-		label.setText("This is a label:");
-		label.setToolTipText("This is the tooltip of this label");
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new GridLayout(1, false));
+		final Group groupStamp = new Group(composite, SWT.TITLE);
+		groupStamp.setText("Inputs:");
+		groupStamp.setLayout(new GridLayout(4, true));
+		groupStamp.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
-		Text text = new Text(container, SWT.NONE);
-		text.setText("This is the text in the text widget");
-		text.setBackground(container.getDisplay().getSystemColor(SWT.COLOR_BLACK));
-		text.setForeground(container.getDisplay().getSystemColor(SWT.COLOR_WHITE));
+		GridData gdFirm = new GridData();
+		gdFirm.grabExcessHorizontalSpace = true;
+		gdFirm.horizontalAlignment = SWT.FILL;
+
+		Label cityLabel = new Label(groupStamp, SWT.NONE);
+		cityLabel.setText("Stadt");
+		final Text cityText = new Text(groupStamp, SWT.BORDER | SWT.SINGLE);
+		cityText.setLayoutData(gdFirm);
+
+		Label aptLabel = new Label(groupStamp, SWT.NONE);
+		aptLabel.setText("Airport");
+		final Text aptText = new Text(groupStamp, SWT.BORDER | SWT.SINGLE);
+		aptText.setLayoutData(gdFirm);
+
+		
+		//city.getTextControl(container).getText();
+		
+		
+		final Button buttonPrintDate = new Button(groupStamp, SWT.CHECK | SWT.LEFT);
+		buttonPrintDate.setText("Datum drucken");
+		buttonPrintDate.setSelection(true);
+
+		final DateTime dateTime = new DateTime(groupStamp, SWT.CALENDAR);
+		dateTime.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (buttonPrintDate.getSelection()) {
+					Calendar date = Calendar.getInstance();
+					date.set(dateTime.getYear(), dateTime.getMonth(), dateTime.getDay());
+				}
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				widgetSelected(e);
+			}
+		});
+		GridData gdDate = new GridData();
+		gdDate.grabExcessHorizontalSpace = true;
+		gdDate.horizontalAlignment = SWT.FILL;
+		dateTime.setLayoutData(gdDate);
+		buttonPrintDate.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (buttonPrintDate.getSelection()) {
+					Calendar date = Calendar.getInstance();
+					date.set(dateTime.getYear(), dateTime.getMonth(), dateTime.getDay());
+				} 
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				widgetSelected(e);
+			}
+		});
+
+
+		
 	}
 
 	@Override
