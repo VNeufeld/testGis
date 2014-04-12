56c9866ea8df36631f5de3badda09b65b02bde97
Valeri Neufeld <valeri.neufeld@arcor.de>
2014-04-12 22:30:46 +0200

    comit

--- com.dev.gis.app/src/com/dev/gis/app/Components.java
+++ com.dev.gis.app/src/com/dev/gis/app/Components.java
@@ -1,11 +1,9 @@
 package com.dev.gis.app;
 
-import com.dev.gis.db.api.IDaoFactory;
 
 public class Components {
 	private static Components instance;
 	
-	private IDaoFactory daoFactory;
 	
 	public Components() {
 		instance = this;
@@ -13,17 +11,6 @@ public class Components {
 
 	public static Components getInstance() {
 		return instance;
-	}
-	public void setDaoFactory(IDaoFactory daoFactory) {
-		this.daoFactory = daoFactory;
-	}
-
-	public void unsetDaoFactory(IDaoFactory daoFactory) {
-		this.daoFactory = null;
-	}
-
-	public IDaoFactory getDaoFactory() {
-		return daoFactory;
 	}
 	
 	
